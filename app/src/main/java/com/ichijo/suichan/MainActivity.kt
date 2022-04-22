ea7b31c85912d84ee3ad542ed2771ccbaff27b23
Kein Pyi Si <keinpyisi@gmail.com>
2022-04-22 23:16:20 +0900

    Krul

--- app/src/main/java/com/ichijo/suichan/MainActivity.kt
+++ app/src/main/java/com/ichijo/suichan/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ichijo.suichan
 import android.os.Build.ID
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import needle.Needle
@@ -11,42 +12,33 @@ import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var editSuitext: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editSuitext=findViewById<EditText>(R.id.editSuitext)
+//        Needle.onBackgroundThread().execute(Runnable {
+//            var data=TalkToSuisei("Hi")
+//            Needle.onMainThread().execute(Runnable { editSuitext.setText(data) })
+//
+//        })
 
-       
-    }
-    fun talkher(conversation: String) = runBlocking {
-        doWorld(conversation)
-    }
-    // Concurrently executes both sections
-    suspend fun doWorld(conversation: String) = coroutineScope { // this: CoroutineScope
-        launch {
-            delay(2000L)
-            var data=TalkToSuisei(conversation)
-
-
-        }
 
 
     }
+
 
     fun TalkToSuisei(conversation:String): String {
 
 
 
         val talkInterface = SuichanBrainInterface.create()
-        Log.e("TAG",conversation+" ")
         val response =talkInterface.getMessage(conversation)
 
         var talk=""
-        val countDownLatch = CountDownLatch(1)
         if (response != null) {
-            Log.e("TAG",conversation+" ")
 
-            Needle.onBackgroundThread().execute {
+
                 var res=response.execute()
                 var suisei=res.body()
                 if (suisei != null) {
@@ -57,7 +49,7 @@ class MainActivity : AppCompatActivity() {
                 }else{
                     talk="Suichan is feeling sick.. Dont want to talk "
                 }
-            }
+
 
 
         }else{
